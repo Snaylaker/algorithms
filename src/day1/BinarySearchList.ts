3a517d781bb1caecea774bcd05a884c731500ff6
pavlocha <mehdihimmid@hotmail.com>
2023-04-09 11:35:49 +0200

    test

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,14 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let lo = 0;
-    let hi = haystack.length;
+    let high = haystack.length;
     do {
-        let m = Math.floor(lo + (hi - lo) / 2);
+        let m = Math.floor((high + lo) / 2);
         if (haystack[m] === needle) return true;
-        if (haystack[m] > needle) {
-            hi = m;
-        } else {
+        if (haystack[m] < needle) {
             lo = m + 1;
         }
-    } while (lo < hi);
+        if (haystack[m] > needle) {
+            high = m - 1;
+        }
+    } while (lo < high);
     return false;
 }
