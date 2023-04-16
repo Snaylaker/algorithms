84423f0046df40b9496cb7311dea4e8fb6cce6c8
pavlocha <mehdihimmid@hotmail.com>
2023-04-16 15:32:11 +0200

    got them stacks with a refractor

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,33 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
-
-}
+        this.length++;
+        this.head = {
+            value: item,
+            prev: this.head ? this.head : undefined,
+        };
+    }
     pop(): T | undefined {
-
-}
+        if (!this.head) {
+            return undefined;
+        }
+        const returnValue = this.head.value;
+        this.head = this.head.prev;
+        this.length--;
+        return returnValue;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
